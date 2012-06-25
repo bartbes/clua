c36e45fc68195534fc7889ff22227af1fe0ed921
Bart van Strien <bart.bes+projects@gmail.com>
2012-06-25 20:04:55 +0200

    Make compiling with c++ possible
    You know, for those systems that can't do c99

--- clua.c
+++ clua.c
@@ -4,9 +4,15 @@
 #include <string.h>
 
 // lua
+#ifdef __cplusplus
+extern "C" {
+#endif
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
+#ifdef __cplusplus
+}
+#endif
 
 // physfs
 #include <physfs.h>
